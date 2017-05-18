38b78831fcf3c83fa274dd34333b27f3bc387e80
Matyas Selmeci <matyas@cs.wisc.edu>
2017-05-18 16:10:33 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup( name='Dask-Condor'
+     , version='0.0.20170518'
+     , description='HTCondor backend for Dask.Distributed'
+     , author='Matyas Selmeci'
+     , author_email='matyas@cs.wisc.edu'
+     , url='https://github.com/matyasselmeci/dask_condor'
+     , packages=['dask_condor']
+     )
